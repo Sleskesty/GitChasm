b65c369fa05abbc5723f436cba2526e352dea146
Sleskesty <Listonryley@gmail.com>
2020-08-03 14:20:18 -0600

    the right stuff

---
+++ src/VMcall.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+
+} from "react-router-dom";
+import "."
+
+function VM() {
+  return (
+    <Router>
+    <div>
+      <header className="VM-header">
+        <a class='call-button' href='./Dialpad'>
+        
+        </a>
+        {/*<a class="dialer-button" href='./about'>
+        </a> */}
+      </header>
+    </div>
+    </Router>
+    )
+}
+
+export default VM;
